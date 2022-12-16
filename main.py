aada8e32d30416636801a87bda3db693ebf14b94
Thomas Tattersfield <118993239+Thomas-Tatt@users.noreply.github.com>
2022-12-16 15:59:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# asks user for input
+User_number = int(input("Please input a positive interger: "))
+# initializes I to 0
+i = 0
+# shows the formula and Calculations
+while i <= 10:
+  print ("{} * {} = {}". format (i, User_number, i * User_number))
+  i += 1 
